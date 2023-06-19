6cd7d1c10d0191bc5af6bc3fd842b347f26d4f01
shanto <hhassibul@gmail.com>
2023-06-19 18:36:37 +0600

    done

--- components/Cta2.jsx
+++ components/Cta2.jsx
@@ -7,9 +7,9 @@ export default function Cta2() {
         <div className="cta-content d-xl-flex align-items-center justify-content-around text-center text-xl-left">
           <div className="content" data-aos="fade-right" data-aos-delay="200">
             <h2>আপনি কি এখনই এপয়েন্টমেন্ট নিতে চাচ্ছেন ?</h2>
-            <h6><i class="fa fa-hand-o-right" aria-hidden="true"></i> আমাদের রয়েছে পুরুষদের জন্য পুরুষ ফিজিওথেরাপিস্ট এবং স্বয়ং সম্পন্ন পুরুষ বিভাগ । 
+            <h6 className="text-start p-2"><i class="fa fa-hand-o-right" aria-hidden="true"></i> আমাদের রয়েছে পুরুষদের জন্য পুরুষ ফিজিওথেরাপিস্ট এবং স্বয়ং সম্পন্ন পুরুষ বিভাগ । 
             </h6>
-            <h6><i class="fa fa-hand-o-right" aria-hidden="true"></i> মহিলাদের জন্য রয়েছে <span className="text-info">মহিলা ফিজিওথেরাপিস্ট</span> ও স্বয়ং সম্পন্ন   <span className="text-info"> আলাদা মহিলা বিভাগ </span>।</h6>
+            <h6 className="text-start p-2"><i class="fa fa-hand-o-right" aria-hidden="true"></i> মহিলাদের জন্য রয়েছে <span className="text-info">মহিলা ফিজিওথেরাপিস্ট</span> ও স্বয়ং সম্পন্ন   <span className="text-info"> আলাদা মহিলা বিভাগ </span>।</h6>
           </div>
           <div
             className="subscribe-btn"
